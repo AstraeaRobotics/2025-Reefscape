adbcb979159c6a78d4f22cf9f3740a6c70f8eeaf
Sobhard <sohumb999@gmail.com>
2025-01-22 17:01:12 -0500

    Fixed Configure Motors

--- src/main/java/frc/robot/subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
@@ -50,6 +52,8 @@ public class AlgaeIntake extends SubsystemBase {
     //DUMMY VALUES
     config.smartCurrentLimit(10);
     config.closedLoopRampRate(10);
+    
+    motor.configure(config, ResetMode.kResetSafeParameters.kResetSafeParameters, PersistMode.kPersistParameters.kPersistParameters);
   }
 
   public void setPivot(double speed){m_pivot.set(speed);}
