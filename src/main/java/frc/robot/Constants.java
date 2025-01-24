fe52c27f0d9a08e0d9e350bacd40383e3a679280
sunny-cq-wang <sophia.cq.wang@gmail.com>
2025-01-24 18:24:18 -0500

    jivin - LED constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,41 +8,7 @@ import edu.wpi.first.math.util.Units;
 
 /** Add your docs here. */
 public class Constants {
-    public static class MotorPortIDConstants {
-        public static final int kLeftAlgaeMotor = 0;
-        public static final int kRightAlgaeMotor = 0;
-        public static final int kPivotAlgaeMotor = 0;
-    }
-
-    public static class DrivebaseModuleConstants {
-        // Physical Constants
-        public static final double kDriveGearRatio = 3.56;
-        public static final double kWheelDiameter = Units.inchesToMeters(3);
-
-        // Conversion Factors
-        public static final int kTurnEncoderPositionFactor = 360;
-        public static final int kTurnEncoderVelocityFactor = 60; // not sure about this
-
-        public static final double kDriveEncoderPositionFactor = (1 / kDriveGearRatio) * 2 * Math.PI * (kWheelDiameter / 2);
-        public static final double kDriveEncoderVelocityFactor = 1/(60 * kDriveGearRatio);
-
-        // PID Constants (change later)
-        public static final double turnKP = 0.004;
-        public static final double turnKI = 0;
-        public static final double turnKD = 0;
-
-        public static final double driveKP = 0.01;
-        public static final double driveKI = 0;
-        public static final double driveKD = 0;
-
-        // FeedForward Constants
-        public static final double turnKV = 0.00005; // 0.31
-        public static final double driveKV = 6.5; // 6.5
-  }
-
-  public static class DrivebaseConstants {
-    // Physical constants
-    public static final double kWheelBase = Units.inchesToMeters(20);/* the distance between the front and rear wheels */
-    public static final double kTrackWidth = Units.inchesToMeters(22); /* the distance between left and right wheels */
+  public static class LedConstants{
+    public static final int ledLength = 0;
   }
 }
