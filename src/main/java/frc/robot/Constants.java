55d18f4775a0059e12467abb6f58314335ac6217
darkmagicianer <jivinsharma02@gmail.com>
2025-01-25 12:47:38 -0500

    LED Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,5 +10,13 @@ import edu.wpi.first.math.util.Units;
 public class Constants {
   public static class LedConstants{
     public static final int ledLength = 0;
+    public static final int kPwmPort = 0;
+        public static final int kLength = 144;
+        public static final double kInterval = Units.secondsToMilliseconds(0.1);
+        public static final double kIntervalTrail = Units.secondsToMilliseconds(0.05);
+        public static final int kRainbowSaturation = 255;
+        public static final int kRainbowValue = 150;
+        public static final int kRainbowIncrement = 150;
+        public static final int kMaxHue = 180;
   }
 }
--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -3,14 +3,16 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import edu.wpi.first.math.MathUtil;
+
+
+import edu.wpi.first.wpilibj.util.Color;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
-import java.util.Timer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.simulation.AddressableLEDSim;
-import java.awt.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LedConstants;
 
@@ -18,21 +20,21 @@ public class LEDSubsystem extends SubsystemBase {
   /** Creates a new LEDSubsystem. */
   boolean enabled = false;
   private boolean isOn = true;
-  private AddressableLED m_led;
-  private final AddressableLEDBuffer m_ledBuffer;
-  private final AddressableLEDSim m_ledSim;
-  private long m_previousTimeFlash=0;
-  private int m_rainbowFirstPixelHue = 30;
+  private AddressableLED led;
+  private final AddressableLEDBuffer ledBuffer;
+  private final AddressableLEDSim ledSim;
+  private long previousTimeFlash=0;
+  private int rainbowFirstPixelHue = 30;
   private int currentTrailIndex = 0;
   int currentIndex = 0;
   int futureIndex = 0;
   public LEDSubsystem() {
-    m_led = new AddressableLED(0);
-    m_ledBuffer = new AddressableLEDBuffer(LedConstants.ledLength);
-    m_led.setLength(m_ledBuffer.getLength());
-    m_led.setData(m_ledBuffer);
-    m_led.start();
-    m_ledSim = new AddressableLEDSim(m_led);
+    led = new AddressableLED(0);
+    ledBuffer = new AddressableLEDBuffer(LedConstants.ledLength);
+    led.setLength(ledBuffer.getLength());
+    led.setData(ledBuffer);
+    led.start();
+    ledSim = new AddressableLEDSim(led);
     Shuffleboard.getTab("DashBoard").addRaw("Addressable Led", this::getLedData).withWidget("Addressable LED");
     setState(LEDStates.kTrailPurple);
   }
@@ -42,6 +44,7 @@ public enum LEDStates {
   kOff,
   kGlowRed,
   kGlowYellow,
+  kGlowPurple,
   kRainbow,
   kintaking,
   kPlacing,
@@ -56,10 +59,124 @@ public void setOutPut(LEDStates state){
   switch (state) {
     case kGlowRed:
     glow(Color.kRed);
+    break;
+    case kPlacing:
+    break;
+    case kRainbow:
+    rainbow();
+    break;
+    case kTrailPurple:
+    trail(Color.kRed, Color.kYellow, 20, 6);
+    break;
+    case kTrailYellow:
+    trail(Color.kYellow, Color.kPurple, 20, 6);
+    break;
+    case kGlowYellow:
+    bigTrail(Color.kYellow, 10, 4);
+    break;
+    case kGlowPurple:
+    bigTrail(Color.kPurple, 10, 4);
+    break;
+    case kFlashYellow:
+    flash(Color.kYellow);
+    break;
+    case kFlashPurple:
+    flash(Color.kPurple);
+    break;
+    case kFlashGreen:
+    flash(Color.kGreen);
+    break;
+    case kBigInterval:
+    bigTrail(Color.kRed, 10, 4);
+    break;
+    default:
+    break;
+  }
+  led.setData(ledBuffer);
+}
+private byte[] getLedData(){
+  return ledSim.getData();
+}
+public void glowRGB(int r, int g, int b){
+  for (int v=0; v < ledBuffer.getLength(); v++){
+    ledBuffer.setRGB(v, r, g, b);
+  }
+
+}
+public void setLEDRGB(int i, int r, int g, int b, double brightness){
+  ledBuffer.setRGB(i, (int)brightness*r, (int)brightness*g, (int)brightness*b);
+
+}
+public void setLED(int i, Color color, double brightness){
+  setLEDRGB(i, (int)(color.red*255), (int)(color.green*255), (int)(color.blue*255), brightness);
+}
+public void glow(Color color){
+  glowRGB((int)(color.red*64), (int)color.blue*64, (int)color.green*64);
+}
+public void rainbow() {
+for (int i=0; i < ledBuffer.getLength(); i++){
+  int hue = 
+  (rainbowFirstPixelHue + (i*180 / ledBuffer.getLength())) % 180;
+  ledBuffer.setHSV(i, hue, 255, 128);}
+  rainbowFirstPixelHue += 2;
+  rainbowFirstPixelHue %= 180;
+}
+
+public void flashRGB(int r, int g, int b) {
+long currentTime= System.currentTimeMillis();
+if ((currentTime - previousTimeFlash) >= LedConstants.kInterval){
+  previousTimeFlash=currentTime;
+  isOn = !isOn;
+}
+if (!isOn){
+  r=0;
+  g=0;
+  b=0;
+}
+for (int i = 0; i < ledBuffer.getLength(); i++){
+  ledBuffer.setRGB(i, r, g, b);
+}
+}
+public void flash(Color color){
+  flashRGB((int)color.red*64, (int)color.green*64, (int)color.blue*64);
+}
+public void trail(Color bgColor, Color movingColor, int trailLength, int speed) {
+  glow(bgColor);
+  for (int i = currentTrailIndex; i < currentTrailIndex + trailLength; i++) {
+    ledBuffer.setLED(i % ledBuffer.getLength(), movingColor);
+  }
+  currentTrailIndex += speed;
+}
+public void fadeRed(){
+  double t = Timer.getFPGATimestamp();
+  for (int i = 0; i < ledBuffer.getLength(); i++) {
+    ledBuffer.setRGB(i, (int) (255*(Math.cos(1.5*t)*0.5+0.5)), 0, 0);
   }
 }
+public void fadeBlue(){
+  double t = Timer.getFPGATimestamp();
+  for (int i = 0; i < ledBuffer.getLength(); i++) {
+    ledBuffer.setRGB(i, 0, 0, (int)(255*(Math.cos(1.5*t)*0.5+0.5)));
+  }
+}
+public void bigTrail(Color color, double interval, int length) {
+  long currentTime = System.currentTimeMillis();
+
+  if (currentTime - previousTimeFlash >= interval) {
+    previousTimeFlash = currentTime;
+    
+    for (int i = 0; i < LedConstants.kLength; i++) {
+      int targetLED = (i + currentIndex) % LedConstants.kLength;
+      setLED(targetLED, color, Math.max((double)i / (LedConstants.kLength * 4) - 0.01, 0.01));
+    }
+    currentIndex++;
+  }
+}
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    if (DriverStation.isDisabled()){
+      setOutPut(LEDStates.kBigInterval);
+    }
   }
 }
