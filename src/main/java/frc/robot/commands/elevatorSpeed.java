508bef12fdae5bf67f283b8fe3997deb848b15ab
Aarush324 <154009086+Aarush324@users.noreply.github.com>
2025-01-26 09:46:59 -0500

    Elevator

--- src/main/java/frc/robot/commands/elevatorSpeed.java
+++ src/main/java/frc/robot/commands/elevatorSpeed.java
@@ -34,7 +34,7 @@ public class elevatorSpeed extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+    m_elevatorSubsystem.setMotorSpeed(0);
   }
 
   // Returns true when the command should end.
