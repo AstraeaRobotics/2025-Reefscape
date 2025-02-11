9d3140ed9b0ce56adb0181449a01e30ffc818b23
Sobhard <sohumb999@gmail.com>
2025-02-11 14:32:40 -0500

    Added Voltage Buttons for Pivot and Intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,11 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.Algae.SetAlgaeIntakeVoltageManually;
+import frc.robot.commands.Algae.SetAlgaePivotVoltageManually;
+import frc.robot.subsystems.AlgaeIntake;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,16 +19,16 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final AlgaeIntake m_algaeIntake = new AlgaeIntake();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final PS4Controller m_controller = new PS4Controller(OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final JoystickButton kTriangle = new JoystickButton(m_controller, PS4Controller.Button.kTriangle.value);
+  private final JoystickButton kCircle = new JoystickButton(m_controller, PS4Controller.Button.kCircle.value);
+  private final JoystickButton kSquare = new JoystickButton(m_controller, PS4Controller.Button.kSquare.value);
+  private final JoystickButton kCross = new JoystickButton(m_controller, PS4Controller.Button.kCross.value);
+
   public RobotContainer() {
-    // Configure the trigger bindings
     configureBindings();
   }
 
@@ -42,13 +42,10 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    kTriangle.whileTrue(new SetAlgaeIntakeVoltageManually(m_algaeIntake, 1));
+    kCircle.whileTrue(new SetAlgaePivotVoltageManually(m_algaeIntake, 1));
+
   }
 
   /**
@@ -56,8 +53,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+  // public Command getAutonomousCommand() {
+
+  // }
 }
